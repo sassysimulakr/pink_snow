c6f57dbd41c351b0f351be7a69c867448e5c8c68
Ihewaz <ihewaz@mail.ru>
2017-04-29 01:10:13 +0300

    добавил парсер
    его нельзя добавлять в итоговую версию!!
    --dev mode

---
+++ js/parser.js
@@ -0,0 +1,297 @@
+const str = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+<meta name="Keywords" content="Агентство переводов Нижний Новгород, перевод текста, перевод текста с английского, перевод с английского на русский, переводчик, синхронный перевод, перевод песен, русский перевод, переводы, перевод текста, перевод английский,
+английский язык перевод, переводчик с английского, переводчик с английского на русский, перевод английского текста, перевод документов,  перевод договоров, перевод инструкций, технический перевод, юридический перевод, медицинский перевод, верстка, перевод сайтов на английский язык" />
+<meta name="Description" content="Агентство переводов Нижний Новгород, перевод текста, перевод текста с английского, перевод с английского на русский, переводчик, синхронный перевод, перевод песен, русский перевод, переводы, перевод текста, перевод английский,
+английский язык перевод, переводчик с английского, переводчик с английского на русский, перевод английского текста, перевод документов,  перевод договоров, перевод инструкций, технический перевод, юридический перевод, медицинский перевод, верстка, перевод сайтов на английский язык" />
+<title>&quot;Привет&quot; на 75 языках мира</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/icon" />
+<link rel="stylesheet" href="llingvcss.css" type="text/css" />
+</head>
+<body class="bodyfon" >
+<table  width="1000" border="0" align="center" cellpadding="0" cellspacing="0" >
+  <tr >
+    <td colspan="3"><table width="100%" border="0"  cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="17" height="120" valign="top" ><img src="images/topleft.png" alt="" width="17" height="120" /></td>
+          <td width="170" height="120"  class="fontop" ><span class="header_1">ООО &quot;ЛингваАрт&quot;</span><br />
+            <span class="header_2">603009 Нижний Новгород,<br />
+            пр. Гагарина, д.39, оф.1</span><br />
+            Телефоны:<br />
+            (831) 461-89-74, 415-24-91<br />
+            E-mail: lingvart@mail.ru</td >
+          <td valign="bottom"    class="fontop" ><table  width="100%" border="0" align="center" cellpadding="0" cellspacing="2" >
+              <tr >
+                <td width="130" height="35" align="center"   nowrap="nowrap"   id="menu1"><a href="index.html" title="О нашем агентстве переводов">НАШЕ АГЕНТСТВО</a></td>
+                <td width="130" id="menu1" align="center" nowrap="nowrap"><a href="translation.html" title="Переводы">ПЕРЕВОДЫ</a></td>
+                <td width="130" id="menu1" align="center" nowrap="nowrap"><a href="price.html" title="Прайс-лист">ПРАЙС-ЛИСТ</a></td>
+                <td width="130" id="menu1" align="center" nowrap="nowrap"><a href="help.html" title="Переводчикам">ПЕРЕВОДЧИКАМ</a></td>
+                <td width="130" id="menu1" align="center" nowrap="nowrap"><a href="contacts.html" title="Заказ перевода">ЗАКАЗ ПЕРЕВОДА</a></td>
+                <td  id="menu1" align="center" nowrap="nowrap"><a href="clients.html" title="Наши клиенты">НАШИ КЛИЕНТЫ</a> </td>
+                </tr>
+            </table></td >
+          <td width="17" valign="top" ><img src="images/toprigh.png" alt="" width="17" height="120" /></td>
+        </tr>
+      </table></td>
+  </tr>
+   <tr >
+    <td  height="265" class="header" colspan="3" valign="top" ><img src="images/logo.png" alt="Агентство переводов - логотип" width="106" height="64" vspace="5"  align="left" />
+    <h3 >АГЕНТСТВО ПЕРЕВОДОВ</h3></td>
+  </tr>
+  <tr >
+    <td colspan="3" valign="top" ><table  width="100%" border="0" class="fontable"  cellpadding="0" cellspacing="10"  >
+        <tr>
+          <td width="200"   valign="top" ><table  width="200" border="0"  cellpadding="0" cellspacing="0">
+              <tr>
+                <td height="36"  class="menuleft"><strong >ГЛАВНОЕ МЕНЮ</strong></td>
+              </tr>
+              <tr>
+                <td class="menuleftc"><a href="index.html" title="О нашем агентстве переводов" >Наше агентство</a> <a href="translation.html" title="Переводы">Переводы</a> <a href="price.html" title="Прайс-лист">Прайс-лист</a> <a href="help.html" title="Переводчикам">Переводчикам</a> <a href="contacts.html" title="Заказ перевода">Заказ переводов</a> <a href="clients.html" title="Наши клиенты">Наши клиенты</a></td>
+              </tr>
+              <tr>
+                <td ><img src="images/blankmbot.png" alt="" width="200" height="7"  border="0" /></td>
+              </tr>
+              <tr>
+                <td ><br />
+                  &nbsp; <br />
+                </td>
+              </tr>
+              <tr>
+                <td height="36"  class="menuleft"><strong >УСЛУГИ ПЕРЕВОДА</strong></td>
+              </tr>
+              <tr>
+                <td class="menuleftc"><a href="interest07.html" title="Перевод документов" >Перевод документов</a> <a href="article03.html" title="Технический перевод">Технический перевод</a> <a href="article09.html" title="Медицинский перевод">Медицинский перевод</a> <a href="urist.html" title="Юридический перевод">Юридический перевод</a> <a href="article07.html" title="Перевод сайтов">Перевод сайтов</a> <a href="dogovor.html" title="Перевод договоров">Перевод договоров</a></td>
+              </tr>
+              <tr>
+                <td ><img src="images/blankmbot.png" alt="" width="200" height="7"  border="0" /></td>
+              </tr>
+              <tr>
+                <td ><br />
+                  &nbsp; <br />
+                </td>
+              </tr>
+              <tr>
+                <td height="36" class="menuleft"><strong >ЭТО ИНТЕРЕСНО</strong></td>
+              </tr>
+              <tr>
+                <td class="menuleftc1"><a href="interest01.html" title="Об иностранных языках" >Высказывания великих<br />людей о языке</a> <a href="interest02.html" title="Носители языка" >Двадцатка самых больших по числу носителей языка</a> <a href="interest03.html" title="Языки народов мира" >Интересные факты о языках народов мира</a> <a href="interest04.html" title="Привет на иностранных языках" >&quot;Привет&quot; на 75 языках народов мира</a> <a href="interest05.html" title="Признайтесь в любви на 87 языках мира" >Признайтесь в любви на 87 языках мира</a> <a href="interest06.html" title="Переводчикам" >Полезные ссылки для переводчиков</a> <a href="interest07.html" title="Нотариальное заверение перевода" >Нотариальное заверение перевода</a> <a href="interest08.html" title="" >Сколько иностранных языков может изучить человек?</a></td>
+              </tr>
+              <tr>
+                <td ><img src="images/blankmbot.png" alt="" width="200" height="7"  border="0" /></td>
+              </tr>
+            </table></td>
+          <td valign="top" ><table  width="100%" border="0" align="left" cellpadding="0" cellspacing="0" >
+              <tr>
+                <td ><h3 align="center" class="stylered">Агентство переводов ЛингваАрт</h3>
+                  <h4 align="center">&quot;Привет&quot; на 75 языках мира</h4>
+                  <p> Отдыхая в другой стране, важно знать хотя бы минимум слов для того, чтобы изъясниться с местными. Самым важным из этого минимального набора слов, естественно, слово - <strong>привет</strong>. Представляем вашему вниманию слово <strong>привет </strong>на 75 <a href="http://www.ivanpobeda.com/post107158783/" target="_blank">языках мира</a>.</p>
+                  <p>Привет на Азербайджанском - Salaam aleihum<br />
+                    Привет на Албанском - Tungjatjeta<br />
+                    Привет на Английском - Hello<br />
+                    Привет на Арабском - Ahlen<br />
+                    Привет на Арабском - Marhaba<br />
+                    Привет на Армянском - Voghdzuyin<br />
+                    Привет на Ассирийском - Shlama<br />
+                    Привет на Африкаанском - Goeie dag<br />
+                    Привет на Башкирском - Kheyerle irte<br />
+                    Привет на Белорусском - Прывитанне<br />
+                    Привет на Бенгальском - Nomoskaar<br />
+                    Привет на Болгарском - Zdraveite<br />
+                    Привет на Боснийском - Zdravo<br />
+                    Привет на Гавайском - Aloha<br />
+                    Привет на Голландском - Hallo<br />
+                    Привет на Греческом - Geia sou<br />
+                    Привет на Грузинском - Gamardjobat<br />
+                    Привет на Датском - God dag<br />
+                    Привет на Еврейском - Shalom<br />
+                    Привет на Египетском - Iiti<br />
+                    Привет на Зулу - Sawubona<br />
+                    Привет на Ингушском - Salam<br />
+                    Привет на Индонезийском - Selamat<br />
+                    Привет на Исландском - Godan daginn<br />
+                    Привет на Испанском - Hola<br />
+                    Привет на Итальянском - Buon giorno<br />
+                    Привет на Казахском - Salam<br />
+                    Привет на Калмыкском - Mendvt<br />
+                    Привет на Каракалпакском - Assalomu alaikum<br />
+                    Привет на Карельском - Terveh<br />
+                    Привет на Киргизском - Salaam matszbe<br />
+                    Привет на Кирибати - Mauri<br />
+                    Привет на языке индейцев Команче - Haa<br />
+                    Привет на Корейском - Annyoung hasimnikka<br />
+                    Привет на Корякском - Mej<br />
+                    Привет на Кхмерском - Sok sabai jie te<br />
+                    Привет на Латинском - Ave<br />
+                    Привет на Латышском - Sveiki<br />
+                    Привет на Литовском - Sveikas<br />
+                    Привет на Люксембургском - Moien<br />
+                    Привет на Македонском - Zdravo<br />
+                    Привет на Мордовском - Shumbrat<br />
+                    Привет на Зимбабвийском - Sawubona<br />
+                    Привет на Немецком - Guten Tag<br />
+                    Привет на Норвежском - Goddag<br />
+                    Привет на Польском - Dzien dobry<br />
+                    Привет на Португальском - Ola<br />
+                    Привет на Прусском - Kails<br />
+                    Привет на Румынском - Buna<br />
+                    Привет на Сербском - Zdravo<br />
+                    Привет на Сицилийском - Sa’benerica<br />
+                    Привет на Словацком - Dobry<br />
+                    Привет на Тайском - Sawatdi<br />
+                    Привет на Татарском - Isenmesez<br />
+                    Привет на Тибетском - Kam sangbo dugay<br />
+                    Привет на Турецком - Merhaba<br />
+                    Привет на Удмуртском - Ziech bures<br />
+                    Привет на Узбекском - Salaam aleikhem<br />
+                    Привет на Украинском - Прывит<br />
+                    Привет на Уэльском - Dydd da<br />
+                    Привет на Финском - Paivaa<br />
+                    Привет на Французском - Bonjour<br />
+                    Привет на Хинди - Namaste<br />
+                    Привет на Хорватском - Zdravo<br />
+                    Привет на Чеченском - Marsha voghiila<br />
+                    Привет на Чешском - Dobry den<br />
+                    Привет на Чувашском - Salam<br />
+                    Привет на Чукотском - Ye’ti<br />
+                    Привет на Шведском - God dag<br />
+                    Привет на Шотландском - Guid mornin<br />
+                    Привет на Эсперанто - Saluton<br />
+                    Привет на Эстонском – Tervist<br />
+                    Привет на Японском - Konnichi wa</p>
+                  </td>
+              </tr>
+          </table></td>
+          <td width="200"   valign="top" ><table  width="200" border="0"  cellpadding="0" cellspacing="0">
+              <tr>
+                <td height="65" align="center"   class="data">
+                <script  language="javascript" type="text/javascript" src="data.js" >
+     </script></td>
+              </tr>
+              <tr>
+                <td >&nbsp;</td>
+              </tr>
+              <tr>
+                <td height="36"  class="menuleft"><strong >СТАТЬИ</strong></td>
+              </tr>
+              <tr>
+                <td class="menuleftc"><a href="article01.html" title="Перевод со словарем" >Перевод со словарем или как пользоваться разговорником за рубежом</a> <a href="article02.html" title="Об изучении иностранного языка" >Быстрое изучение иностранного языка - правда или обман?</a> <a href="article03.html" title="Особенности перевода технических текстов" >Особенности перевода технических текстов</a> <a href="article04.html" title="Перевод и понимание иностранного языка" >Отличие перевода и понимания иностранного языка</a> <a href="article05.html" title="Изучение английского языка с помощью книг" >Полезные советы по изучению иностранных языков</a> <a href="article06.html" title="Ручной и автоматический перевод текстов" >Ручной и автоматический перевод текстов</a> <a href="article07.html" title="Перевод сайтов на английский язык" >Перевод сайтов на английский язык</a> <a href="article08.html" title="Перевод рекламы – высший пилотаж переводчика" >Перевод рекламы – высший пилотаж переводчика</a> <a href="articles.html" title="Статьи о переводах" ><em><strong><span class="styleblue">Все статьи о переводах &gt;&gt;</span></strong></em> </a></td>
+              </tr>
+              <tr>
+                <td ><img src="images/blankmbot.png" alt="" width="200" height="7"  border="0" /></td>
+              </tr>
+              <tr>
+                <td  ><img src="images/reclama2.gif" alt="Переводы с иностранных языков" width="200" height="98" vspace="10"  border="0" /> </td>
+              </tr>
+              <tr>
+                <td height="36" class="menuleft"><strong >РЕКЛАМА</strong></td>
+              </tr>
+              <tr>
+                <td class="menuleftc1"><a href="http://www.labirint.ru/" title="Словари для переводчиков" target="_blank" >Любые словари для переводчиков. Продажа в Нижнем Новгороде</a> <a href="http://www.booklib.ru/" title="Книги на английском и немецком языке" target="_blank" >Литература на английском и немецком языках</a> <a href="http://langford.ru/" title="Курсы английского языка в Нижнем Новгороде" target="_blank" >Курсы английского языка в Нижнем Новгороде. Стажировка за рубежом</a> <a href="http://www.lingvapro.ru" title="Обучение иностранным языкам в Нижнем Новгороде" target="_blank" >Обучение иностранным языкам в Нижнем Новгороде</a> <a href="http://www.sp2010.ru/" title="Продажа и установка бассейнов" target="_blank" >Продажа и установка бассейнов</a></td>
+              </tr>
+              <tr>
+                <td ><img src="images/blankmbot.png" alt="" width="200" height="7"  border="0" /></td>
+              </tr>
+              <tr>
+                <td height="8"></td>
+              </tr>
+              <tr>
+                <td align="center" class="menuleftc2"><a href="http://www.sp2010.ru" title="Продажа и установка бассейнов" target="_blank"><img src="images/animation.gif" alt="Продажа и установка бассейнов" width="200" height="150"  border="0"  /></a></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td colspan="3" bgcolor="#ffffff"><hr size="1" noshade="noshade" /></td>
+  </tr>
+  <tr >
+    <td  colspan="3" valign="top" bgcolor="#ffffff" ><p align="center">
+        <!--LiveInternet counter-->
+            <script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='http://counter.yadro.ru/hit?t52.6;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet: показано число просмотров и"+
+" посетителей за 24 часа' "+
+"border=0 width=88 height=31><\/a>")//--></script>
+            <!--/LiveInternet-->&nbsp;
+        <!-- begin of Top100 logo -->
+<a href="http://top100.rambler.ru/top100/"><img src="images/banner-8.gif" alt="Rambler's Top100" width="88" height="31" border="0" /></a>
+<!-- end of Top100 logo -->&nbsp;
+<!--Rating@Mail.ru COUNTEr--><script language="JavaScript" type="text/javascript"><!--
+d=document;var a='';a+=';r='+escape(d.referrer)
+js=10//--></script><script language="JavaScript1.1" type="text/javascript"><!--
+a+=';j='+navigator.javaEnabled()
+js=11//--></script><script language="JavaScript1.2" type="text/javascript"><!--
+s=screen;a+=';s='+s.width+'*'+s.height
+a+=';d='+(s.colorDepth?s.colorDepth:s.pixelDepth)
+js=12//--></script><script language="JavaScript1.3" type="text/javascript"><!--
+js=13//--></script><script language="JavaScript" type="text/javascript"><!--
+d.write('<a href="http://top.mail.ru/jump?from=1131825"'+
+' target=_top><img src="http://d5.c4.b1.a1.top.list.ru/counter'+
+'?id=1131825;t=56;js='+js+a+';rand='+Math.random()+
+'" alt="Рейтинг@Mail.ru"'+' border=0 height=31 width=88/><\/a>')
+if(11<js)d.write('<'+'!-- ')//--></script><noscript><a
+target=_top href="http://top.mail.ru/jump?from=1131825"><img
+src="http://d5.c4.b1.a1.top.list.ru/counter?js=na;id=1131825;t=56"
+border=0 height=31 width=88
+alt="Рейтинг@Mail.ru"/></a></noscript><script language="JavaScript" type="text/javascript"><!--
+if(11<js)d.write('--'+'>')//--></script>&nbsp;<!--/COUNTER-->
+<!-Begin Counter.NN.RU v2.0 code->
+<script language="JavaScript" type="text/javascript">
+<!--
+document.write("<a href=\"http://counter.nn.ru/?18403\">"+
+"<IMG src=\"http://counter.nn.ru/cgi-bin/counter.cgi?18403"+
+
+",,"+top.document.referrer+"\" width=\"88\" height=\"31\" border=\"0\" alt=\"Counter.NN.RU\"></a>");
+//-->
+</script>
+<noscript>
+<a href="http://counter.nn.ru/?18403">
+<img src="http://counter.nn.ru/cgi-bin/counter.cgi?18403"
+width="88" height="31" border="0" alt="NN counter top100" /></a>
+</noscript>
+<!-End Counter.NN.RU code->
+      </p></td>
+  </tr>
+  <tr >
+    <td colspan="3"><table width="100%" border="0"  cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="17" height="80"  ><img src="images/bottomleft.png" alt="" width="17" height="123" /></td>
+          <td  bgcolor="#ffffff"><p align="center" class="stylered"> Copyright © 2006 -<script language="javascript" type="text/javascript"><!--
+var d = new Date();
+document.write(d.getFullYear());
+//--></script><strong> ООО «ЛингваАрт»</strong></p>
+            <h6 align="center">Ключевые слова: переводы Нижний Новгород, перевод с английского, технический перевод, перевод текстов, перевод с немецкого, иностранные языки</h6></td >
+          <td width="17"  ><img src="images/bottomrigh.png" alt="" width="17" height="123" /></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+<!-- begin of Top100 code -->
+<script id="top100Counter" type="text/javascript" src="http://counter.rambler.ru/top100.jcn?1551753"></script>
+<noscript>
+<img src="http://counter.rambler.ru/top100.cnt?1551753" alt="" width="1" height="1" border="0" />
+</noscript>
+<!-- end of Top100 code -->
+</body>
+</html>
+`
+
+let reg = /- (\D+?)<br \/>/g;
+
+let match;
+let final = '';
+
+while ((match = reg.exec(str)) !== null) {
+  final += `${match[1]}|`
+}
+
+
+console.log(final.slice(0, -1));
